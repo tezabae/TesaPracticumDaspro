e02cfbe5d72f1c0c44d9a72f70e896fb0c4ae97a
tezabae <tesalonikabenila@gmail.com>
2025-10-09 17:25:20 +0700

    Attempt 1

---
+++ ifCetakKRSNoPresensi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ifCetakKRSNoPresensi {
+    // This class is intentionally left empty.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Print KRS SIAKAD without attendance data");
+        System.out.println("---------------------------------------");
+        System.out.println("Have the tuition fee been paid? (Y/N)");
+        boolean uktLunas = sc.nextLine().equalsIgnoreCase("Y");
+        if (uktLunas) {
+            System.out.println("UKT payment confirmed.");
+            System.out.println("KRS now can be printed.");
+        } else {
+            System.out.println("UKT payment not confirmed.");
+            System.out.println("KRS cannot be printed.");
+
+
+            
+        }
+    }
+}
