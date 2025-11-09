8cf810f23d8cb0bdb8938e96ad7a48d1ed94c179
tezabae <tesalonikabenila@gmail.com>
2025-11-09 21:06:35 +0700

    commit 1

---
+++ Jobsheet9/arraynumber27.java
@@ -0,0 +1,17 @@
+package Jobsheet9;
+
+public class arraynumber27 {
+  public static void main(String[] args) {
+        int[] num = new int[4];
+
+        num[0] = 5;
+        num[1] = 12;
+        num[2] = 7;
+        num[3] = 20;
+
+        System.out.println("Array contents:");
+        for (int i = 0; i < num.length; i++) {
+            System.out.println("Element " + i + ": " + num[i]);
+        }
+    }
+}
