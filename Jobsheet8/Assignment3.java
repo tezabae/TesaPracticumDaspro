1f204569fe30d31cd5d38b7eaf72fc5bc30e7390
tezabae <tesalonikabenila@gmail.com>
2025-11-09 21:48:36 +0700

    ass3

---
+++ Jobsheet8/Assignment3.java
@@ -0,0 +1,20 @@
+package Jobsheet8;
+import java.util.Scanner;
+public class Assignment3 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=n; j++){
+                if (i == 1 || i == n || j == 1 || j == n){
+                    System.out.print(n + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
