5e674eee7f97b81640c50a6af69ebb2cda7d2c36
tezabae <tesalonikabenila@gmail.com>
2025-11-11 08:26:48 +0700

    task1 array2

---
+++ Jobsheet11/CinemaWithScanner.java
@@ -0,0 +1,20 @@
+package Jobsheet11;
+import java.util.Scanner;
+public class CinemaWithScanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int row, coloumn;
+        String name, next;
+
+        String [][] audience = new String [4][2];
+
+        While (true) {
+            
+
+        }
+
+
+    }
+    
+}
