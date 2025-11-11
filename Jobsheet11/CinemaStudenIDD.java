0a544dc9f15c46bb61894efba15481cda9cfbf68
tezabae <tesalonikabenila@gmail.com>
2025-11-11 10:06:47 +0700

    modify 2

--- Jobsheet11/CinemaStudenIDD.java
+++ Jobsheet11/CinemaStudenIDD.java
@@ -14,10 +14,15 @@ public class CinemaStudenIDD {
         audience [2][0] = "Eka";
         audience [2][1] = "Farhan";
         audience [3][0] = "Gisel";
+        audience [3][1] = "Hana";
 
-        System.out.printf("%s \t \t %s\n", audience[0][0], audience[0][1]);
-        System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
-        System.out.printf("%s \t \t %s\n", audience[2][0], audience[2][1]);
-        System.out.printf("%s \t \t %s\n", audience[3][0], audience[3][1]);
+        System.out.println(audience.length);
+        System.out.println(audience[0].length);
+        System.out.println(audience[1].length);
+        System.out.println(audience[2].length);
+        System.out.println(audience[3].length);
+
+
+
     }
 }
