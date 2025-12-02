8c7874a85f8f8920ca8dcc39b7c5765c88b98481
tezabae <tesalonikabenila@gmail.com>
2025-12-02 07:58:39 +0700

    task 1

---
+++ Jobsheet12/Gratitude_StudentIDNumber.java
@@ -0,0 +1,12 @@
+package Jobsheet12;
+
+public class Gratitude_StudentIDNumber {
+    public static void sayThankyou() {
+        System.out.println("Thankyou for being the best Teacer in the world. \n" +
+            "you inspired me a love for learning and made me feel like i feel like i could asked you anything." );
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+    
+}
